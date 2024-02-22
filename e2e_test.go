dc329d2292c145a2b5a8713c07660ad1862bca20
garmr <pdixon117@gmail.com>
2024-02-22 11:06:30 -0800

    adjusted error formatting

--- e2e_test.go
+++ e2e_test.go
@@ -2,7 +2,6 @@ package algeneva
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,20 +11,15 @@ func TestNormalizationAllStrategies(t *testing.T) {
 	for country, strategy := range Strategies {
 		for i, s := range strategy {
 			results, pass, err := TestStrategyNormalization(s)
-			if !assert.NoError(t, err, "%s[%d]: ", country, i) || pass {
+			if !assert.NoError(t, err, "%s[%d]: failed", country, i) || pass {
 				continue
 			}
 
-			var msg []string
 			for _, r := range results {
 				if !r.Pass {
-					msg = append(msg, fmt.Sprintf("%s: %s", r.Name, r.Msg))
+					assert.Fail(t, fmt.Sprintf("%s[%d]: %s: %s", country, i, r.Name, r.Msg))
 				}
 			}
-
-			// Even though we know the test failed by this point, we still use assert for
-			// formatting
-			assert.True(t, pass, "%s[%d] failed:\n\t%s", country, i, strings.Join(msg, "\n\t"))
 		}
 	}
 }
